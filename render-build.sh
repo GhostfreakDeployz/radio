0ff1732701078eb21297bd66dddd8093d08f0d58
GhostfreakDeployz <miguellimadias80@gmail.com>
2025-01-19 21:51:25 -0300

    Deploy Flask on Vercel (AGAINNNNNNN)

---
+++ render-build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+apt-get update && apt-get install -y \
+    build-essential \
+    libssl-dev \
+    libffi-dev \
+    python3-dev \
+    python3-pip \
+    libxml2-dev \
+    libxslt1-dev \
+    zlib1g-dev
